41cb9f3d790d3107c5eff54af9db2c930184ca68
opilane <opilane@HP-CZC2349HV2>
2025-10-14 13:10:51 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Elektrikulu.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Elektrikulu.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<Usage> Usages { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
